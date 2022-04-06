3c28c1ec9f4bcd2e79efad743ebb66ac7d2d5992
MadJack1906 <cho.suhoond.74@gmail.com>
2022-04-06 17:32:59 +0800

    Initial Files

---
+++ index.js
@@ -0,0 +1,12 @@
+const upperCaseBase = 65; // 65 is the number of the 'A' in the ASCII TABLE
+const lowerCaseBase = 97; // 97 is the number of 'a' in the ASCII TABLE
+const regex = /\d|[~!@#$%^&*()_+\-=\[\]{}:";'<>?,./]/g
+
+function shiftIt(){
+    let input = document.getElementById("input").value
+    let output = document.getElementById("output")
+    let cipherText = "";
+    let index = input.length - 1
+
+    
+}
